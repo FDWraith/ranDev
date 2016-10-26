7946abe13606357ceefba9066be2c96737f25be2
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-26 11:38:56 -0400

    I think we finished most of the work. We just need to figure some of the instructions

--- main.c
+++ main.c
@@ -8,7 +8,38 @@
 #include <string.h>
 
 int main(){
-  
-
+  srand(time(NULL));
+  int ary[10];
+  printf("Generating Random Numbers:\n");
+  int i =0;
+  for(;i<10;i++){
+    ary[i] = rand();
+    printf("\tRandom %d: %d\n",i,ary[i]);
+  }
+  printf("Creating file.txt\n");
+  int fd = open("file.txt", O_CREAT | O_EXCL | O_RDWR, 0644);
+  printf("File Descriptor:[%d]\n",fd);
+  if( fd < 0){
+    printf("Error found while attempting to create file\n");
+  }
+  printf("Writing into file\n");
+  int err = write( fd, ary, sizeof(ary));
+  if( err < 0){
+    printf("Error found: %s\n",strerror(errno));
+  }
+  err = 0;
+  close(fd);
+  printf("Reading file into new array\n");
+  int ary2[10];
+  fd = open("file.txt", O_RDWR);
+  err = read(fd,ary2,sizeof(ary2));
+  if(err < 0){
+    printf("Error found: %s\n",strerror(errno));
+  }
+  printf("Verification of Array:\n");
+  for( i =0; i< 10; i++){
+    printf("\tRandom %d: %d\n",i,ary2[i]);
+  }
+  close(fd);
   return 0;
 }
