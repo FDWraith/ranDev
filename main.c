bcb62700a35be4f1efa1f9f6cc39ab5f7ef67678
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-26 16:25:20 -0400

    We tried our best, but rndInt refuses to work with write for some reason. The random numbers are generated, but write refuses to accept it.

--- main.c
+++ main.c
@@ -6,28 +6,56 @@
 
 #include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-void rndInt(int * loc);
+int rndInt(int rnd);
 
 int main(){
+  /*
+  int rndSeed = open("/dev/urandom", O_RDONLY);
+  printf("Testing rndInt\n");
+  int p = rndInt(rndSeed);
+  printf("RandomInteger:[%d]\n",p);
+  int q[2];
+  q[0] = rndInt(rndSeed);
+  q[1] = rndInt(rndSeed);
+  printf("RandomArray:[%p]\n",q);
+  printf("First Element:[%d]\n",q[0]);
+  printf("Second Element:[%d]\n",q[1]);
+  */
+  //printf("something happened\n");
+  
   srand(time(NULL));
   int ary[10];
   printf("Generating Random Numbers:\n");
   int i =0;
-  rndInt(ary);
+  //rndInt(ary);
   for(;i<10;i++){
-    //ary[i]= rand();
+    ary[i]= rand();
+    //ary[i] = rndInt(rndSeed);
     printf("\tRandom %d: %d\n",i,ary[i]);
   }
   
   printf("Creating file.txt\n");
-  int fd = open("file.txt", O_CREAT | O_EXCL | O_RDWR, 0644);
+  //umask(0);
+  int fd = open("file.txt", O_CREAT | O_RDWR, 0666);
   printf("File Descriptor:[%d]\n",fd);
   if( fd < 0){
     printf("Error found while attempting to create file\n");
   }
+  if( fd < 0){
+    printf("Error found while attempting to open file\n");
+  }
   printf("Writing into file\n");
-  int err = write( fd, ary, sizeof(ary));
+  //printf("File Descriptor:[%d]\n",fd);
+  //printf("Array Pointer:[%p]\n",ary);
+  //printf("Array First Element:[%d]\n",ary[0]);
+  //printf("Array Last Element[%d]\n", ary[9]);
+  //printf("Size of Array:[%lu]\n",sizeof(ary));
+  int err = write(fd,ary,sizeof(ary));
+  //printf("Segmentation fault (core dumped)\n");
+  //printf("Error\n");
   if( err < 0){
     printf("Error found: %s\n",strerror(errno));
   }
@@ -48,7 +76,18 @@ int main(){
   return 0;
 }
 
-void rndInt(int * loc){
-  int fd = open("/dev/urandom", O_RDONLY);
-  read(fd,loc,40);
+int rndInt(int fd){
+  //
+  if(fd < 0){
+    printf("Invalid file descriptor\n");
+    return -1;
+  }else{
+    int * p;
+    int err = read(fd,p,4);
+    if( err < 0){
+      printf("Error while getting random integers\n");
+    }
+    //close(fd);
+    return abs(*p);
+  }
 }
